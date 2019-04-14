d495e7cddff6759f0c1820d9c3231259daa84d2f
samir <nazurdinov.s@gmail.com>
2019-04-14 16:57:33 +0500

    implemented Base62 library to generate slugs for urls

--- app/Http/Controllers/API/ShorturlsController.php
+++ app/Http/Controllers/API/ShorturlsController.php
@@ -4,27 +4,24 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Requests\StoreShorturls;
 use App\Shorturl;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Tuupola\Base62;
 
 
 class ShorturlsController extends Controller
 {
-
-
     public function index()
     {
         $shortUrls = Shorturl::all();
         return response()->json($shortUrls, 200);
     }
 
-    public function store(StoreShorturls $request)
+    public function store(StoreShorturls $request, Base62 $base62)
     {
+
         $validated = $request->validated();
 
-        $slug = '2KCnBr2'. rand(0,100); // TODO: Generate unique slug
-
-        $validated['slug'] = $slug;
+        $validated['slug'] = $this->uniqueSlug($base62);
 
         $shortUrl = Shorturl::create($validated);
         return response()->json($shortUrl, 201);
@@ -37,12 +34,10 @@ class ShorturlsController extends Controller
         return response()->json($shortUrl, 200);
     }
 
-    public function update(StoreShorturls $request, Shorturl $shorturl)
+    public function update(StoreShorturls $request, Shorturl $shorturl, Base62 $base62)
     {
         $validated = $request->validated();
-        $slug = '2KCnBr2'. rand(0,100); // TODO: Generate unique slug
-
-        $validated['slug'] = $slug;
+        $validated['slug'] = $this->uniqueSlug($base62);
 
         $shorturl->update($validated);
         return response()->json($shorturl, 200);
@@ -53,5 +48,18 @@ class ShorturlsController extends Controller
         $shorturl->delete();
 
         return response()->json(null, 204);
+    }
+
+    public function uniqueSlug(Base62 $base62)
+    {
+        while (true){
+            $slug = $base62->encode(random_bytes(5));
+
+            if(Shorturl::where('slug', $slug)->doesntExist()){
+                break;
+            }
+        }
+
+        return $slug;
     }
 }
